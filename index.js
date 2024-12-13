c0225b4847d3c5111c822866d43fb42fba2885c8
euanlimzx <euanlimzx@gmail.com>
2024-12-12 16:38:47 -0800

    handle disconnections + use socketID as roomcode

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+const PORT = process.env.PORT || 3000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -20,31 +21,30 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  let socketRoom = null; //this code works because we assume each connection will only have one room
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    socketRoom = room;
+    io.to(room).emit("room-status", "A new user joined the room! ");
   });
 
-  socket.on("join-room", (room) => {
-    console.log("joined room");
-    socket.join(room);
-    io.to(room).emit("join-room", "A new user joined the room! ");
+  socket.on("disconnect", () => {
+    if (socketRoom) {
+      io.to(socketRoom).emit("room-status", "User disconnected ");
+    }
   });
 
   socket.on("keystroke", (keyEvent, room) => {
     if (room) {
-      console.log(keyEvent);
       io.to(room).emit("keystroke", keyEvent);
     }
   });
 
   socket.on("keep-alive", (message, room) => {
-    console.log("keep alive received");
     io.to(room).emit("keep-alive", "pong");
   });
 });
 
-server.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
+server.listen(PORT, () => {
+  console.log("server running at ", PORT);
 });
