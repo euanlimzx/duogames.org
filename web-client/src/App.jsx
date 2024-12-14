ba4465267f6ae7a971214e7b79033b71c0331ede
euanlimzx <euanlimzx@gmail.com>
2024-12-13 20:06:10 -0800

    successful migration to react + chakra

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -3,43 +3,86 @@ import { socket } from "./socket";
 import { ConnectionState } from "./components/ConnectionState";
 import { ConnectionManager } from "./components/ConnectionManager";
 import { Events } from "./components/Events";
-import { MyForm } from "./components/MyForm";
-import { HStack } from "@chakra-ui/react";
+import { Text, HStack, Box } from "@chakra-ui/react";
 
 export default function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [roomCode, setRoomCode] = useState(null);
+  const [keysPressed, setKeysPressed] = useState({});
 
   useEffect(() => {
     function onConnect() {
       setIsConnected(true);
+      setRoomCode(socket.id);
+      console.log(roomCode);
+      socket.emit("join-room", roomCode);
     }
 
     function onDisconnect() {
       setIsConnected(false);
+      setRoomCode(null);
+      setEvents([]);
     }
 
-    function onFooEvent(value) {
-      setFooEvents((previous) => [...previous, value]);
+    function onKeystroke(keyEvent) {
+      const value = `keystroke received: ${keyEvent.keyDir} ${keyEvent.keyCode}`;
+      setEvents((previous) => [...previous, value]);
     }
+
+    function onRoomUpdate(update) {
+      setEvents((previous) => [...previous, update]);
+    }
+
+    const handleKeyDown = (event) => {
+      if (!keysPressed[event.key]) {
+        setKeysPressed((prev) => ({ ...prev, [event.key]: true }));
+        socket.emit(
+          "keystroke",
+          { keyDir: "keydown", keyCode: event.keyCode },
+          roomCode
+        );
+      }
+    };
+
+    // Handle keyup event
+    const handleKeyUp = (event) => {
+      setKeysPressed((prev) => ({ ...prev, [event.key]: false }));
+      socket.emit(
+        "keystroke",
+        { keyDir: "keyup", keyCode: event.keyCode },
+        roomCode
+      );
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
 
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
-    socket.on("foo", onFooEvent);
+    socket.on("keystroke", onKeystroke);
+    socket.on("room-status", onRoomUpdate);
 
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
-      socket.off("foo", onFooEvent);
+      socket.off("keystroke", onKeystroke);
+      socket.off("room-status", onRoomUpdate);
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
     };
-  }, []);
+  }, [roomCode, keysPressed]);
 
   return (
     <div className="App">
-      <ConnectionState isConnected={isConnected} />
-      <Events events={fooEvents} />
-      <ConnectionManager />
-      <MyForm />
+      <HStack>
+        <Box>
+          {roomCode && <Text color="white">Your room code is: {roomCode}</Text>}
+          <ConnectionState isConnected={isConnected} />
+          <ConnectionManager />
+        </Box>
+        <Events events={events} />
+      </HStack>
     </div>
   );
 }
