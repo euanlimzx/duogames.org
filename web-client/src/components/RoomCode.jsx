4a6783d20b32e7f8cb40a4b01f31e5cb7e8bc279
euanlimzx <euanlimzx@gmail.com>
2024-12-17 12:48:25 -0800

    first iteration of glitch text done

--- web-client/src/components/RoomCode.jsx
+++ web-client/src/components/RoomCode.jsx
@@ -1,31 +1,45 @@
-import { Text } from "@chakra-ui/react";
-export function RoomCode() {
-  const roomCode = "0EcDJNmx46QdNFzXAAAP".split("");
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+import {
+  Text,
+  useToast,
+  Tooltip,
+  Box,
+  Flex,
+  Button,
+  Stack,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { FaCheckCircle } from "react-icons/fa";
+import { socket } from "../socket";
+export function RoomCode({ roomCode }) {
+  const toastId = "copy-toast";
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const SOCKET_ID_LENGTH = 20;
+  function getRandomLetter() {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    return letters[randomIndex];
+  }
+  const [code, setCode] = useState(
+    [...Array(SOCKET_ID_LENGTH)].map(getRandomLetter)
+  );
 
-  const [code, setCode] = useState(roomCode);
-  let iteration = -1;
+  const toast = useToast();
 
   useEffect(() => {
+    let iteration = 0;
+    let interval = null;
     function scrambleText() {
-      function getRandomLetter() {
-        const randomIndex = Math.floor(Math.random() * letters.length);
-        return letters[randomIndex];
-      }
-      setCode(Array.from({ SOCKET_ID_LENGTH }, getRandomLetter));
-      const interval = setInterval(() => {
-        setCode(
+      interval = setInterval(() => {
+        setCode((code) =>
           code.map((letter, index) => {
-            if (index <= iteration) {
-              return roomCode[index];
+            if (roomCode && index <= iteration) {
+              return roomCode.split("")[index];
             } else {
               return getRandomLetter();
             }
           })
         );
-        if (iteration != -1) {
-          iteration += 1 / 2; // this if condition is what triggers the stop of the animation
+        if (roomCode) {
+          iteration += 4 / 3;
         }
         if (iteration >= SOCKET_ID_LENGTH) {
           clearInterval(interval);
@@ -33,7 +47,85 @@ export function RoomCode() {
       }, 50);
     }
     scrambleText();
-  }, []);
+    return () => clearInterval(interval);
+  }, [roomCode]);
 
-  return <Text>Hello</Text>;
+  return (
+    <Stack direction={{ base: "column", md: "row" }} gap="1.5rem">
+      <Tooltip label="Click to copy">
+        <Text
+          color="black"
+          borderRadius="1rem"
+          fontSize="5xl"
+          fontWeight="semibold"
+          mx="2rem"
+          px="1rem"
+          cursor="pointer"
+          _hover={{ bg: "gray.100" }}
+          onClick={() => {
+            if (roomCode) {
+              navigator.clipboard.writeText(roomCode);
+              if (!toast.isActive(toastId)) {
+                toast({
+                  title: "Room Code Copied!",
+                  status: "success",
+                  duration: 3000,
+                  isClosable: true,
+                  id: toastId,
+                  // containerStyle: {
+                  //   width: 10,
+                  //   maxWidth: "400px",
+                  //   display: "flex",
+                  //   justifyContent: "center",
+                  //   alignItems: "center",
+                  //   padding: 3,
+                  // },
+                  render: ({ title, onClose }) => (
+                    <Box
+                      p={2}
+                      bg="#2F8559"
+                      borderRadius="lg"
+                      textAlign="center"
+                      marginBottom={5}
+                      textColor={"white"}
+                    >
+                      <Flex
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                        padding={3}
+                        gap={7}
+                      >
+                        <Text fontWeight={700} textAlign={"center"}>
+                          {title}
+                        </Text>
+                        <FaCheckCircle strokeWidth={2} />
+                      </Flex>
+                    </Box>
+                  ),
+                });
+              }
+            }
+          }}
+        >
+          {code.join("")}
+        </Text>
+      </Tooltip>
+      <Button
+        size="xl"
+        borderRadius="5rem"
+        p="1.75rem"
+        mx={{ base: "3rem", md: "0rem" }}
+        onClick={() => {
+          socket.disconnect();
+        }}
+        background="black"
+        color="white"
+        _hover={{ bg: "gray.700" }}
+      >
+        <Text fontSize="2xl" fontWeight="semibold">
+          Disconnect
+        </Text>
+      </Button>
+    </Stack>
+  );
 }
