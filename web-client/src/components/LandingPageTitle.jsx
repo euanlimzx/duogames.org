bdf47f8c1b1a823208d2a10a3f577673796e2214
euanlimzx <euanlimzx@gmail.com>
2025-01-22 23:18:33 -0800

    copy change

--- web-client/src/components/LandingPageTitle.jsx
+++ web-client/src/components/LandingPageTitle.jsx
@@ -38,9 +38,7 @@ export function LandingPageTitle() {
           textColor="gray.600"
           maxW={{ base: "90%", md: "60%" }}
         >
-          <Text>
-            Long distance just got alot a little easier (and a lot more fun!)
-          </Text>
+          <Text>Long distance just a little easier (and a lot more fun!)</Text>
           <Text>
             Play co-op browser games, even if you're not on the same laptop!
           </Text>
